589ac54b4399f47ebf5ed3f2972c6c351d31ed6b
jesselessa <jessica.elessa@gmail.com>
2024-11-26 06:41:19 +0100

    handled protected routes

--- client/src/layouts/dashboardLayout/DashboardLayout.jsx
+++ client/src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,7 +1,24 @@
 import "./dashboardLayout.css";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
 
 const DashboardLayout = () => {
+  //! The 'useAuth()' hook provides information about the current auth state, as well as helper methods to manage the current active session.
+  // 'isLoaded' : a boolean that until Clerk loads and initializes, will be set to false. Once Clerk loads, it will be set to true.
+  // 'userId' : the current user ID
+  const { isLoaded, userId } = useAuth();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoaded && !userId) {
+      navigate("/sign-in");
+    }
+  }, [isLoaded, userId, navigate]);
+
+  if (!isLoaded) return "Loading...";
+
   return (
     <div className="dashboardLayout">
       <div className="menu">MENU</div>
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,12 @@
 import "./home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Link to="/dashboard">Dashboard</Link>
+    </div>
+  );
 };
 
 export default Home;
