8f015ca4954d9c7d73b34764b9e88b65c8fc6c6b
jesselessa <jessica.elessa@gmail.com>
2024-11-27 05:21:26 +0100

    fixed homepage bg image and chatbox text animation & completed media queries

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,25 @@
 import "./home.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
 
 const Home = () => {
+  const [typingStatus, setTypingStatus] = useState("john");
+
   return (
     <div className="home">
       <img src="/orbital.png" alt="orbital" className="orbital" />
       {/* Left */}
       <div className="left">
         <h1>JESS&nbsp;AI</h1>
-        <h2>Supercharge your creativity and productivity</h2>
+        <h2>
+          Your friendly mini assistant for big ideas and quick&nbsp;answers
+        </h2>
         <h3>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-          porro praesentium commodi magni odio maiores velit, expedita atque
-          saepe ducimus?
+          Jess AI is a compact and intuitive chatbot, using Google Gemini AI.
+          <br />
+          Smart, fast, and always ready, it makes your life simpler, one chat at
+          a time&nbsp;!
         </h3>
         <Link to="/dashboard">Get Started</Link>
       </div>
@@ -24,8 +31,62 @@ const Home = () => {
           <div className="bg-container">
             <div className="bg"></div>
           </div>
+
           {/* Animated image*/}
           <img src="/bot.png" alt="bot" className="bot" />
+
+          {/* Chatbox */}
+          <div className="chatbox">
+            <img
+              src={
+                typingStatus === "john"
+                  ? "/john.jpeg"
+                  : typingStatus === "jane"
+                  ? "/jane.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+            />
+
+            {/* Animated text */}
+            <TypeAnimation
+              sequence={[
+                "John : What is the capital of France ?",
+                2000, // A callback function is added after each animation
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot : Paris is the capital of France",
+                2000,
+                () => {
+                  setTypingStatus("jane");
+                },
+                "Jane : What time is it ?",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot : It's 3 PM",
+                2000,
+                () => {
+                  setTypingStatus("john");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="terms">
+        <img src="/logo.png" alt="logo" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
         </div>
       </div>
     </div>
